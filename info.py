98d6bdb6454ac370249b7d2c4353ad7778b2e95b
Rk2xbotz <vipsrmwebseries@gmail.com>
2025-11-30 13:40:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -161,7 +161,7 @@ LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
 PROTECT_CONTENT = is_enabled("PROTECT_CONTENT", False)
 SPELL_CHECK = is_enabled("SPELL_CHECK", True)
 LINK_MODE = is_enabled("LINK_MODE", True)
-TMDB_API_KEY = environ.get("TMDB_API_KEY", "8ea253c57a45285f5fd2b85ca425a819")
+TMDB_API_KEY = environ.get("TMDB_API_KEY", "")
 
 # Online Streaming And Download
 STREAM_MODE = bool(environ.get("STREAM_MODE", True))  # Set True or Flase
